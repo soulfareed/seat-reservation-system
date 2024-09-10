c025cdc93f26879cd3016b824a1f0ee7a86e301d
Md fareed <fareedzaman501@gmail.com>
2024-09-11 02:23:36 +0530

    seat-reservation-backend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request,Response } from "express";
+import seatRoutes from './routes'
+import bodyParser from 'body-parser'
+import { bundlerModuleNameResolver } from "typescript";
+
+
+const app = express()
+
+const PORT = 3000;
+
+app.use(bodyParser.json())
+
+app.use('/api',seatRoutes)
