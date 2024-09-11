7458dcb1f8a57de2f8a57eebbd75ceb6b8cdca2f
Md fareed <fareedzaman501@gmail.com>
2024-09-12 02:08:20 +0530

    adddb

---
+++ backend/src/controller/seatController.ts
@@ -0,0 +1,3 @@
+import { Request,Response } from "express";
+import {db} from '../db'
+import {Seat} from '../models/seat'
---
+++ backend/src/models/seat.ts
@@ -0,0 +1,6 @@
+ export interface Seat{
+    id: 'number',
+    seat_number: 'number',
+    row_number: 'number',
+    status: 'avilable' | 'booked'
+ }
