5c76e4b70319d3ff602d97d0fe850ef1026722be
Md fareed <fareedzaman501@gmail.com>
2024-09-13 19:14:18 +0530

    edit api

--- backend/src/controller/seatController.ts
+++ backend/src/controller/seatController.ts
@@ -16,22 +16,28 @@ export const reserveSeats = async (req: Request, res: Response) => {
   const { seatCount } = req.body;
   console.log(req.body);
   res.send({ message: "success" });
-  //   if (seatCount < 1 || seatCount > 7) {
-  //     return res
-  //       .status(400)
-  //       .json({ message: "You can reserve between 1 and 7 seats at a time" });
-  //   }
+  if (seatCount < 1 || seatCount > 7) {
+    return res
+      .status(400)
+      .json({ message: "You can reserve between 1 and 7 seats at a time" });
+  }
 
-  //   try {
-  //     const [availableSeats] = await db.query<RowDataPacket[]>(
-  //       'SELECT * FROM seats WHERE status = "available" ORDER BY row_number, seat_number'
-  //     );
-  //     if (availableSeats.length < seatCount) {
-  //       return res.status(400).json({ message: "Not enough available seats" });
-  //     }
+  try {
+    const [availableSeats] = await db.query<RowDataPacket[]>(
+      'SELECT * FROM seats WHERE status = "available" ORDER BY row_number, seat_number'
+    );
+    if (availableSeats.length < seatCount) {
+      return res.status(400).json({ message: "Not enough available seats" });
+    }
 
-  //     //   let reservedSeats: Seat[]=
-  //   } catch (err) {
+    let reservedSeats: Seat[] = [];
+    const groupedByRow = groupedByRow(availableSeats);
 
-  //   }
+    for (const rowSeats of groupedByRow) {
+      if (rowSeats.length >= seatCount) {
+        reservedSeats = rowSeats.slice(0, seatCount);
+        break;
+      }
+    }
+  } catch (err) {}
 };
