c852233d40fc8086f228c8ec7353357cfb6a3bc5
Md fareed <fareedzaman501@gmail.com>
2024-09-13 19:33:44 +0530

    fix bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 // import router from "./routes";
 import bodyParser from "body-parser";
 // import { bundlerModuleNameResolver } from "typescript";
-import router from "./routes";
+import seatRoutes from "./routes";
 
 const app = express();
 
@@ -10,8 +10,8 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.use("/api", router);
+app.use("/api", seatRoutes);
 
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log("server is running on http://localhost:${PORT}");
 });
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,15 @@
-import { Router, Request, Response } from "express";
-import mysql from "mysql2/promise";
+import { Router } from "express";
+// import mysql from "mysql2/promise";
 import { getSeats, reserveSeats } from "./controller/seatController";
 
 const router = Router();
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "seat-reservation",
-});
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "seat-reservation",
+// });
 
 router.get("/seats", getSeats);
 
